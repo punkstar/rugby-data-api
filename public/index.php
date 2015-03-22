ea986a60cdb7d6dcbb8cb4cff8d0baeb4604cf15
Nick Jones <nick@nicksays.co.uk>
2015-03-22 17:11:33 +0000

    Add home page listing available resources

--- public/index.php
+++ public/index.php
@@ -9,4 +9,32 @@ $config = [
 $app = new Silex\Application($config);
 $app->mount('/fixtures', new \Punkstar\RugbyFeedService\Controller\LeagueController());
 
+$app->get('/', function () {
+    $links = [];
+
+    $leagues = [
+        new \Punkstar\RugbyFeed\League\Aviva()
+    ];
+
+    foreach ($leagues as $league) {
+        $league_url = 'fixtures/' . $league->getUrlKey();
+        $links[] = sprintf(
+            '<li><a href="%s">%s</a></li>',
+            $league_url,
+            $league_url
+        );
+
+        foreach ($league->getTeams() as $team) {
+            $team_url = 'fixtures/' . $league->getUrlKey() . '/' . $team->getUrlKey();
+            $links[] = sprintf(
+                '<li><a href="%s">%s</a></li>',
+                $team_url,
+                $team_url
+            );
+        }
+    }
+
+    return "<h1>Available Resources</h1> " . join("\n", $links);
+});
+
 $app->run();
